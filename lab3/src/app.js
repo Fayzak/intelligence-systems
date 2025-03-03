4fdfc2a93333550e1ddb17deca341af10f7fecab
Fayzak <gildem19@yandex.ru>
2025-03-03 20:11:06 +0300

    lab3: init

---
+++ lab3/src/app.js
@@ -0,0 +1,60 @@
+require('./logging.js')('info')
+
+const Controller = require('./controller')
+const Agent = require('./agent') // Импорт агента
+const VERSION = 7 // Версия сервера
+
+const teamName = "Traktor" // Имя команды
+
+let enemyTeamName = "Really"
+// let enemyAgent = new Agent()
+
+// Функция для разбора именованных параметров
+function parseArgs(args) {
+    const result = {}
+    args.slice(2).forEach(arg => {
+        if (arg.startsWith('--')) {
+            const [key, value] = arg.slice(2).split('=')
+            result[key] = value !== undefined ? value : true
+        }
+    })
+    return result
+}
+
+// Получаем именованные параметры
+const args = parseArgs(process.argv)
+
+const enemy_x = 15
+const enemy_y = -10
+
+const x = args.x === undefined ? -15 : args.x
+const y = args.y === undefined ? -10 : args.y
+const angle = args.angle === undefined ? 45 : args.angle
+
+let agent = new Agent() // Создание экземпляра агента
+agent.setController(new Controller())
+require('./socket')(agent, teamName, VERSION) //Настройка сокета
+agent.move(x, y)
+// let agent1 = new Agent() // Создание экземпляра агента
+// require('./socket')(agent1, teamName, VERSION) //Настройка сокета
+// agent1.socketSend("move", `${-10} ${-10}`) // Размещение игрока на поле
+// let agent2 = new Agent() // Создание экземпляра агента
+// require('./socket')(agent2, teamName, VERSION) //Настройка сокета
+// agent2.socketSend("move", `${-10} ${-5}`) // Размещение игрока на поле
+// let agent3 = new Agent() // Создание экземпляра агента
+// require('./socket')(agent3, teamName, VERSION) //Настройка сокета
+// agent3.socketSend("move", `${-10} ${0}`) // Размещение игрока на поле
+// let agent4 = new Agent() // Создание экземпляра агента
+// require('./socket')(agent4, teamName, VERSION) //Настройка сокета
+// agent4.socketSend("move", `${-10} ${5}`) // Размещение игрока на поле
+// let agent5 = new Agent() // Создание экземпляра агента
+// require('./socket')(agent5, teamName, VERSION) //Настройка сокета
+// agent5.socketSend("move", `${-10} ${10}`) // Размещение игрока на поле
+
+// require('./socket')(enemyAgent, enemyTeamName, VERSION)
+// enemyAgent.socketSend("move", `${-enemy_x} ${-enemy_y}`)
+
+// setTimeout(() => {
+//
+//
+// }, 1000)
---
+++ lab3/src/controller.js
@@ -0,0 +1,164 @@
+const Flags = require('./flags')
+const Utils = require('./utils')
+
+Searcher = {
+    getDirection() {
+        const nextDirection = this.directions[this.currentDirectionIndex]
+
+        this.currentDirectionIndex = (this.currentDirectionIndex + 1) % this.directions.length
+
+        return nextDirection
+    },
+
+    reset() {
+        this.currentDirectionIndex = 0
+    },
+
+    directions: [45, 45, -72 - 90, -72, -72, -72, -72],
+    currentDirectionIndex: 0
+}
+
+class Controller {
+    constructor() {
+        this.actions = [
+            {action: "run", goal: "frt"},
+            {action: "run", goal: "frb"},
+            {action: "run", goal: "flb"},
+            {action: "run", goal: "flt"},
+            {action: "run", goal: "fc"},
+            {action: "kick", goal: "gr"}
+        ]
+
+        this.currentActionIndex = 0
+    }
+
+    getCommand(position, angle, flags, gameObjects) {
+
+        this.switchIfComplited(position, angle, flags, gameObjects)
+
+        const {action: action, ...actionParameters} = this.actions[this.currentActionIndex]
+
+        switch (action) {
+            case "run":
+
+                const visibleDestionation = flags.find(flag => flag.name === actionParameters["goal"])
+                const destination = Flags[actionParameters["goal"]]
+
+                return this.run(visibleDestionation, destination, position, angle)
+
+            case "kick":
+
+                const visibleGate = flags.find(flag => flag.name === actionParameters["goal"])
+                const gate = Flags[actionParameters["goal"]]
+                const ball = gameObjects.find(gameObject => gameObject.name === "b")
+
+                return this.kick(visibleGate, gate, ball, position, angle)
+
+        }
+
+        return ["stay"]
+    }
+
+    switchIfComplited(position, angle, flags, gameObjects) {
+
+        const {action: action, ...actionParameters} = this.actions[this.currentActionIndex]
+
+        switch (action) {
+            case "run":
+
+                const visibleDestionation = flags.find(flag => flag.name === actionParameters["goal"])
+                const destination = Flags[actionParameters["goal"]]
+
+                if (this.checkRunComplited(visibleDestionation, destination, position, angle)) {
+                    this.currentActionIndex = (this.currentActionIndex + 1) % this.actions.length
+                }
+
+                break
+            case "kick":
+
+                break
+        }
+
+    }
+
+    getDirectionAndDistance(visibleDestionation, destination, position, angle) {
+
+        if (!position || !angle) {
+
+            if (!visibleDestionation) {
+                return [null, null]
+            }
+
+            return [visibleDestionation.angle, visibleDestionation.d]
+        }
+
+        const beta = Utils.calculateRelativeAngle(position, destination)
+        const direction = Utils.getAnglesDifference(beta, angle) 
+        const distance = Utils.distance(position, destination)
+
+        return [direction, distance]
+
+    }
+
+    checkRunComplited(visibleDestionation, destination, position, angle) {
+
+        const [direction, distance] = this.getDirectionAndDistance(visibleDestionation, destination, position, angle)
+
+        if (!direction || !distance) {
+            return false
+        }
+
+        return distance < 3
+
+    }
+
+    run(visibleDestionation, destination, position, angle, flags) {
+
+        const [direction, distance] = this.getDirectionAndDistance(visibleDestionation, destination, position, angle)
+
+        if (!direction || !distance) {
+            return ["turn", Searcher.getDirection()]
+        }
+        Searcher.reset()
+
+        if (Math.abs(direction) > 10 * (1 + 10 / distance)) {
+            return ["turn", direction]
+        }
+
+        return ["dash", 100]
+
+    }
+
+    kick(visibleGate, gate, ball, position, angle) {
+
+        if (!ball) {
+            return ["turn", Searcher.getDirection()]
+        }
+        Searcher.reset()
+
+        if (ball.d < 1) {
+            const [gateDirection, gateDistance] = this.getDirectionAndDistance(visibleGate, gate, position, angle)
+
+            if (!gateDirection || !gateDistance) {
+                if (position && position.x > 0) {
+                    return ["kick", 10, 45] 
+                }
+                return ["kick", 10, -45] 
+            }
+
+            return ["kick", 100, gateDirection]
+        }
+
+        return this.run(ball, ball, position, angle)
+
+    }
+
+    onHear(tick, sender, message) {
+        if (message.includes("goal") && this.actions[this.currentActionIndex].action === "kick") {
+            this.currentActionIndex = (this.currentActionIndex + 1) % this.actions.length
+        }
+    }
+
+}
+
+module.exports = Controller
